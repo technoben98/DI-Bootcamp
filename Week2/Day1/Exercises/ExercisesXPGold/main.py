46504b9cfd9f396c570c8e54eea43abcabc3210f
technoben98 <technoben98@gmail.com>
2023-09-03 15:56:54 +0300

    Adding Exercise XP Gold

--- Week2/Day1/Exercises/ExercisesXPGold/main.py
+++ Week2/Day1/Exercises/ExercisesXPGold/main.py
@@ -1,1 +1,17 @@
-print("Hello World\n"*4,"I love pyton\n"*4)
+#1
+print("\nHello World"*4,"\nI love pyton"*4)
+
+#2
+
+month = int(input("Choose month \"from 1 to 12\"\n"))
+
+if month >=3 and month < 6:
+    print("Now it's Spring")
+elif month >=6 and month < 9:
+    print("Now it's Summer")
+elif month >= 9 and month < 12:
+    print("Now it's Autumn")
+elif month == 12 or month == 1 or month == 2:
+    print("Now it's Winter")
+else:
+    print("Wrong month")
