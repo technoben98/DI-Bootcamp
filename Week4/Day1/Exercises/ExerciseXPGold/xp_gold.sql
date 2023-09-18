147ca502db29a5456dd86de67ff662ae81fab788
technoben98 <technoben98@gmail.com>
2023-09-18 14:32:50 +0300

    Adding Exercise XP Gold

---
+++ Week4/Day1/Exercises/ExerciseXPGold/xp_gold.sql
@@ -0,0 +1,4 @@
+select * from student;
+select * from student order by last_name limit 4;
+select * from student order by birth_date desc limit 1;
+select * from student order by last_name offset 2 limit 3;
