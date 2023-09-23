1515d5184719010d515ebe963a24f0275d420c7f
technoben98 <technoben98@gmail.com>
2023-09-23 21:05:11 +0300

    Adding Challenges#2

---
+++ Week2/Day5/Exercise/challenges#2.py
@@ -0,0 +1,36 @@
+# Exercise 1
+
+
+# print("*".center(5))
+# print(("*"*3).center(5))
+# print(("*"*5).center(5))
+
+# print("*".rjust(5))
+# print(("*"*2).rjust(5))
+# print(("*"*3).rjust(5))
+# print(("*"*4).rjust(5))
+# print(("*"*5).rjust(5))
+
+# print("*")
+# print(("*"*2))
+# print(("*"*3))
+# print(("*"*4))
+# print(("*"*5))
+
+# print(("*"*5).rjust(5))
+# print(("*"*4).rjust(5))
+# print(("*"*3).rjust(5))
+# print(("*"*2).rjust(5))
+# print("*".rjust(5))
+
+# Exercise 2
+# program for sorting from lower to higher
+# my_list = [2, 24, 12, 354, 233]
+# for i in range(len(my_list) - 1):
+#     minimum = i 
+#     for j in range( i + 1, len(my_list)):
+#         if(my_list[j] < my_list[minimum]):
+#             minimum = j
+#             if(minimum != i):
+#                 my_list[i], my_list[minimum] = my_list[minimum], my_list[i]
+# print(my_list)
