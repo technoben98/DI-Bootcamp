b7c628daf9a921f21663e8a67b39f8332ce68a33
technoben98 <technoben98@gmail.com>
2023-10-21 19:44:47 +0300

    Adding Exercise XP Gold

---
+++ Week5/Day4/Exercises/ExerciseXPGold/script.js
@@ -0,0 +1,44 @@
+// Exercise 1
+
+// let numbers = [123, 8409, 100053, 333333333, 7]
+
+// for (number in numbers){
+//     // console.log(numbers[number])
+//     if(numbers[number]%3 == 0){
+//         console.log(true)
+//     } else {
+//         console.log(false)
+//     }
+// }
+
+// Exercise 2
+
+// let guestList = {
+//     randy: "Germany",
+//     karla: "France",
+//     wendy: "Japan",
+//     norman: "England",
+//     sam: "Argentina"
+// }
+
+// let userName = prompt("Your name")
+
+// if (userName in guestList){
+//     alert(`Hi! I'm ${userName}, and I'm from ${guestList[userName]}.`)
+// } else {
+//     alert("Hi! I'm a guest.")
+// }
+
+// Exercise 3
+
+// let age = [20,5,12,43,98,55];
+// let sum = 0;
+// let highestAge = 0;
+// for (let i = 0; i < age.length; i++){
+//     sum+= age[i]
+//     if (highestAge < age[i]){
+//         highestAge = age[i];
+//     }
+// }
+// console.log(sum)
+// console.log(highestAge)
