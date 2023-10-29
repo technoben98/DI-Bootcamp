c32d3a267112e12e9beebacc8897968d1c3c517c
technoben98 <technoben98@gmail.com>
2023-10-29 12:41:18 +0200

    Adding Lesson

---
+++ Week7/Day1/Lesson/script.js
@@ -0,0 +1,39 @@
+// Exercise 1
+
+// let myObj = {
+//   name: "John",
+//   lastName: "Doe",
+//   age: 25,
+//   friends: ["Mark", "Lucie", "Ana"],
+// };
+
+// let keys = Object.keys(myObj);
+// let values = Object.values(myObj);
+
+// console.log(
+//   `Number of keys: ${keys.length}. Number of values: ${values.length}.`
+// );
+
+// let myArr = Object.entries(myObj);
+
+// myArr.forEach((entry, index) => {
+//   const position = index + 1;
+//   console.log(
+//     `The ${position}# key is : ${entry[0]} The ${position}# value is : ${entry[1]}`
+//   );
+// });
+
+// Exercise 2
+
+// class Rabbit {
+//   constructor(type) {
+//     this.type = type;
+//   }
+//   speak(line) {
+//     console.log(`The ${this.type} rabbit says '${line}'`);
+//   }
+// }
+// let killerRabbit = new Rabbit("killer");
+// let blackRabbit = new Rabbit("black");
+// killerRabbit.speak("Caaaaarrroooootttt");
+// blackRabbit.speak("I'm want carrot");
