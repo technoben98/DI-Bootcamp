77ed3d0ea16d82ce775f880cf1b2fadffc1381b4
technoben98 <technoben98@gmail.com>
2023-11-15 13:48:43 +0200

    Daily Challenge

---
+++ Week9/Day1/DailyChallenge/carousel/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+// import { Carousel } from "react-responsive-carousel";
+import Carousel from "./Carousel.js";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Carousel />
+      </header>
+    </div>
+  );
+}
+
+export default App;
